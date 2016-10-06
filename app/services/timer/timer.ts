995170fef77cab9f3714a3fa9bc4391292f681f7
Craig Vincent <craig@accentdesign.co.uk>
2016-10-06 22:26:25 +0100

    Fix bugs with timer

--- app/services/timer/timer.ts
+++ app/services/timer/timer.ts
@@ -5,24 +5,25 @@ export class Timer {
   private started: Date;
   private stopped: Date;
   private endTime: Date;
-  private duration: int;
-  private remaining: int;
+  private duration: Number;
+  private remaining: Number;
   private timer;
 
-  constructor(duration: int) {
+  constructor(duration: Number) {
     this.duration = duration*1000;
   }
 
   public start():void {
     this.started = new Date();
     this.endTime = new Date(this.started.getTime() + this.duration);
-    this.timer = setInterval(this.checkRemaining().bind(this), 100);
+    this.timer = setInterval(this.checkRemaining.bind(this), 100);
   }
 
   checkRemaining() {
     this.remaining = ((this.endTime-new Date())/1000);
     if (this.remaining <= 0) {
       this.stop();
+      this.remaining = 0;
     }
   }
 
